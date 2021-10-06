0a0b0c3cbfc23daa2263a9cd9ebac2f35e7459bb
dqvo13 <91294936+dqvo13@users.noreply.github.com>
2021-10-05 20:37:03 -0700

    Add files via upload

---
+++ decode2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+long decode2(long x, long y, long z) {
+	// x -> %rdi, y -> %rsi, z -> %rdx
+	long result = 0;		// result -> %rax
+	y -= z;					// subq %rdx, %rsi
+	x *= y;					// imulq %rsi, %rdi
+	result = y;				// movq %rsi, %rax
+	result = result << 63;	// salq $63, %rax
+	result = result >> 63;	// sarq $63, %rax
+	result ^= x;			// xorq %rdi, %rax
+	
+	return result;			// ret
+}
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+long decode2(long x, long y, long z);
+
+int main() {
+	long x = 1;
+	long y = 3;
+	long z = 4;
+
+	printf("decode2 returns: %li", decode2(x, y, z));
+
+	return 0;
+}
