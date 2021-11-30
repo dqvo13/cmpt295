212a61ad4dff66458b789ccf164859f7493b3bc0
dqvo13 <91294936+dqvo13@users.noreply.github.com>
2021-11-29 21:49:02 -0800

    Add files via upload

---
+++ Assn9-files/Q2/lsearch_2.c
@@ -0,0 +1,12 @@
+// Filename: lsearch_2.c
+// Description: A more efficient implementation of the linear search.
+
+int lsearch_2(int *A, int n, int target) {
+
+    // Question 2 a.  Put your code below 
+    
+
+
+}  // lsearch_2
+
+
